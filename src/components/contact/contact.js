881028a8d8e071a146b6b067d4b31343ced12910
Parth0415 <parthmali1597@gmail.com>
2023-05-15 22:56:08 -0400

    Add CSS to components

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,8 +1,17 @@
 import React from "react";
+import "./contact.css"
 
 function Contact(){
     return(
-        <h1>CONTACT</h1>
+        <div>
+            <h1>Contact me</h1>
+            <form className="contactForm">
+                <input placeholder="Name" className="inputField"/>
+                <input placeholder="Email" className="inputField"/>
+                <textarea placeholder="Message" rows= "10" className="inputField"/>
+                <div className="submitButton">Submit</div>
+            </form>
+        </div>
     )
 }
 
--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -6,6 +6,8 @@ import project3 from "../assets/images/project3.png";
 import project4 from "../assets/images/project4.png";
 import project5 from "../assets/images/project5.png";
 import project6 from "../assets/images/project6.png";
+import github from "../assets/images/github.png";
+import web from "../assets/images/web.png";
 function Portfolio() {
   return (
     <div>
@@ -14,36 +16,42 @@ function Portfolio() {
         <ProjectComponent
           title="Project 1"
           img={project1}
+          github = "https://github.com/AshKetchumAndPikachu/PokemonBattle"
           link="https://ashketchumandpikachu.github.io/PokemonBattle/"
           tech="Javascript, HTML, CSS"
         />
         <ProjectComponent
           title="Project 2"
           img={project2}
+          github ="https://github.com/Project2Group6/Shift_Wiz"
           link="https://shift-wiz.herokuapp.com/"
           tech="Javascript, HTML, CSS"
         />
         <ProjectComponent
           title="Project 3"
           img={project3}
+          github ="https://github.com/Parth0415/Fundamental_Coding_Quiz"
           link="https://parth0415.github.io/Fundamental_Coding_Quiz/"
           tech="Javascript, HTML, CSS"
         />
            <ProjectComponent
           title="Project 4"
           img={project4}
+          github ="https://github.com/Parth0415/Code-Refactor"
           link="https://parth0415.github.io/Code-Refactor/"
           tech="Javascript, HTML, CSS"
         />
            <ProjectComponent
           title="Project 5"
           img={project5}
+          github ="https://github.com/Parth0415/Parth-Daily-Work-Day-Scheduler"
           link="https://parth0415.github.io/Parth-Daily-Work-Day-Scheduler/"
           tech="Javascript, HTML, CSS"
         />
            <ProjectComponent
           title="Project 6"
           img={project6}
+          github ="https://github.com/Parth0415/Secure-Password-Generator"
           link="https://parth0415.github.io/Secure-Password-Generator/"
           tech="Javascript, HTML, CSS"
         />
@@ -56,12 +64,18 @@ function ProjectComponent(props) {
   return (
     <div className="projectTile">
       <img src={props.img} alt="project" />
-      <div className="projectBody">
 
+      <div className="projectBody">
       <h2>{props.title}</h2>
-      <a href={props.link}>Gitub</a>
+      
+      <div className="projectLinks">
+      <a href={props.github}><img className="projectLink" src={github} alt= "github"/></a>
+      <a href={props.link}><img className="projectLink" src={web} alt= "web"/></a>
+      </div>
+
       <p>{props.tech}</p>
       </div>
+
     </div>
   );
 }

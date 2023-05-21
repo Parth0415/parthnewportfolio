46b035965efebcd72053449b1cb175ae911c33c7
Parth0415 <parthmali1597@gmail.com>
2023-05-21 08:02:14 -0400

    Edit form validation

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,62 +1,117 @@
 import React, { Component } from "react";
-import "./contact.css"
+import "./contact.css";
 
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      name: "",
+      message: "",
+      fieldEmpty: { name: false, email: false, message: false },
+      emailValid: true,
+    };
+  }
 
-class Contact extends Component{
+  errors = { nameError: "", emailError: "", msgError: "" };
+  componentDidUpdate() {
+    if (this.state.fieldEmpty.email) {
+      this.errors.emailError = "Email cannot be empty";
+    } else {
+      this.errors.emailError = "";
+    }
+    if (this.state.fieldEmpty.message) {
+      this.errors.msgError = "Message cannot be empty";
+    } else {
+      this.errors.msgError = "";
+    }
+    if (this.state.fieldEmpty.name) {
+      this.errors.nameError = "Name cannot be empty";
+    } else {
+      this.errors.nameError = "";
+    }
+    if (!this.state.emailValid) {
+      // console.log(this.state.emailValid)
+      this.errors.emailError = "Email not valid";
+    } else {
+      this.errors.emailError = "";
+    }
+  }
 
-    constructor(props){
-        super(props);
-        this.state = {
-            email:'',
-            name:"",
-            message:"",
-            fieldEmpty:'',
-            emailValid:false
-        }
+  validateField = (e) => {
+    let validEmail = this.state.emailValid;
+    let emptyField = this.state.fieldEmpty;
+    if (e.target.value === "") {
+      emptyField = { ...emptyField, [e.target.name]: true };
+    } else {
+      emptyField = { ...emptyField, [e.target.name]: false };
+    }
+    if (e.target.name === "email") {
+      const regexEmail = new RegExp(
+        /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+      );
+      validEmail = regexEmail.test(e.target.value);
+      //console.log("this is", validEmail)
     }
 
-    validateField = (fieldName, value)=>{
-        let validEmail = this.state.emailValid
-        let emptyField = this.state.fieldEmpty
-        if(value == ""){
-            emptyField=fieldName;
-        }
-        if(fieldName == "email"){
-            validEmail = value.match(/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
-            )
-        }
+    this.setState({
+      emailValid: validEmail,
+      fieldEmpty: emptyField,
+    });
 
-        this.setState({
-            emailValid:validEmail,
-            fieldEmpty:emptyField
-        })
+    // check emailValid from state to show the errros
+  };
 
-        // check emailValid from state to show the errros
-    }
+  handleUserInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState(
+      {
+        [name]: value,
+      },
+      () => {
+        this.validateField(e);
+      }
+    );
+  };
 
-    handleUserInput = (e)=>{
-        const name = e.target.name
-        const value = e.target.value
-        this.setState({
-            [name]:value
-        },()=>{this.validateField(name, value)})
-    }
-
-
-    render (){
-       
-    return(
-        <div>
-            <h1>Contact me</h1>
-            <form className="contactForm">
-                <input placeholder="Name" value={this.state.name} className="inputField" name="name"/>
-                <input placeholder="Email" value={this.state.email} className="inputField" name="email"/>
-                <textarea placeholder="Message" value={this.state.message} rows= "10"  name="messages" className="inputField"/>
-                <div className="submitButton">Submit</div>
-            </form>
-        </div>
-    )
-}
-
+  render() {
+    return (
+      <div>
+        <h1>Contact me</h1>
+        <form className="contactForm">
+          <input
+            placeholder="Name*"
+            value={this.state.name}
+            onChange={this.handleUserInput}
+            className="inputField"
+            name="name"
+            onBlur={this.validateField}
+          />
+          <p className="contectError">{this.errors.nameError}</p>
+          <input
+            placeholder="Email*"
+            value={this.state.email}
+            className="inputField"
+            onChange={this.handleUserInput}
+            name="email"
+            onBlur={this.validateField}
+          />
+          <p className="contectError">{this.errors.emailError}</p>
+          <textarea
+            placeholder="Message*"
+            value={this.state.message}
+            rows="10"
+            onBlur={this.validateField}
+            onChange={this.handleUserInput}
+            name="message"
+            className="inputField"
+          />
+          <p className="contectError">{this.errors.msgError}</p>
+          <div className="submitButton">Submit</div>
+        </form>
+      </div>
+    );
+  }
 }
 export default Contact;
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,8 +1,18 @@
 import React from "react";
 import './footer.css';
+import github from "../assets/images/github.png"
+import linkedin from "../assets/images/linkedin.png"
+import twitter from "../assets/images/twitter.png"
 function Footer(){
     return(
-        <footer>&copy; Copyright Parth Mali 2023</footer>
+        <footer>
+            <div className="footerContainer">
+                <a className="footerLink" href="https://github.com/Parth0415"><img src={github} alt = "github"/></a>
+                <a className="footerLink" href="https://www.linkedin.com/in/parth-mali-677a59234/"><img src={linkedin} alt = "linkedin"/></a>
+                <a className="footerLink" href="https://twitter.com/ParthMa62916849"><img src={twitter} alt = "twitter"/></a>
+            </div>
+            <div>&copy; Copyright Parth Mali 2023</div>
+        </footer>
     )
 }
 
